b3365f20fbdca8ee86f0fb41523a60bada4885ca
Christopher Brown <io@henrian.com>
2017-05-01 18:11:50 -0500

    Add license to project config

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject the/parsatron "0.0.8-SNAPSHOT"
   :description "Clojure parser combinators"
+  :license {:name "Eclipse Public License"
+            :url "https://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2227"]]
