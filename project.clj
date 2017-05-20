3efebb6ddaacd0c957936f25133c9e47a9b8b21d
Christopher Brown <io@henrian.com>
2017-05-20 00:12:29 -0500

    Set version to 0.0.8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject chbrown/parsatron "0.0.8-SNAPSHOT"
+(defproject chbrown/parsatron "0.0.8"
   :description "Clojure parser combinators"
   :url "https://github.com/chbrown/parsatron"
   :license {:name "Eclipse Public License"
